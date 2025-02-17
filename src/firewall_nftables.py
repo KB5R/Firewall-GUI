791fcfbbcd08e2a9fc6a30ac2080a859a785467e
Mr.Freeman <101461532+KB5R@users.noreply.github.com>
2025-02-17 17:09:48 +0300

    Add while True

--- src/firewall_nftables.py
+++ src/firewall_nftables.py
@@ -15,14 +15,11 @@
 
 #! /usr/bin/python3
 
-
 import os
 import subprocess
 from datetime import datetime
 
-
-
-# Функция по отключению fi
+# Remove iptables and firewalld
 def run_remove_firewall():
     try:
         subprocess.run(["bash", "remove_firewall.sh"], check=True)
@@ -32,11 +29,9 @@ def run_remove_firewall():
 
 # run_remove_firewall()
 
-
-
-
-
-
+#---------------------------------------------------------------------------------------
+# Полная очистка правил nftable
+# ВНИМАНИЕ /etc/nftables.conf не будет удалятся
 def clear_firewall():
     confirm = input('Подтвердите вы уверены что хотите очистить firewall (Y/N) ')
     if confirm.lower() == 'y':
@@ -46,23 +41,26 @@ def clear_firewall():
             print("Правила успешно очищены.")
         except subprocess.CalledProcessError as e:
             print(f"Ошибка при выполнении команды: {e}")
-
-
-
+#---------------------------------------------------------------------------------------
 
 
 def main():
-    print("\nМеню:")
-    print("1. Важно для стабильной работы nftables! Удаление сторонних firewall (iptables, firewalld)")
-    print("2. Очистить  firewall (nftables)")
-    print("3. Выйти")
+    while True:
+        print("\nМеню:")
+        print("1. Важно для стабильной работы nftables! Удаление сторонних firewall (iptables, firewalld)")
+        print("2. Очистить firewall (nftables)")
+        print("3. Выйти")
 
+        number = int(input('Сделайте выбор сценария: '))
+        if number == 1:
+            run_remove_firewall()
+        elif number == 2:
+            clear_firewall()
+        elif number == 3:
+            print("Выход из программы.")
+            break  # Выход
+        else:
+            print('Некорректный выбор, попробуйте ещё раз.')
+
+# Запуск основной функции
 main()
-
-number = int(input('Сделайте выбор сценария: '))
-if number == 1:
-    run_remove_firewall()
-elif number == 2:
-    clear_firewall()
-else:
-    print('Некорректный выбор, попробуйте ещё раз.')
